56aaeeea5a24508a93feab908482d5359350fadb
iloveiu3k <125787338+iloveiu3k@users.noreply.github.com>
2024-04-29 18:17:24 +0700

    dsa
    das

--- Assets/Data/Character/Player/PlayerPunCallBack.cs
+++ Assets/Data/Character/Player/PlayerPunCallBack.cs
@@ -43,7 +43,7 @@ public class PlayerPunCallBack :Player
         playerMovement.SetJumpForce(heigh);
     }
     [PunRPC]
-    public void SetHighJumpOther()
+    public void SetHighJumpOther(int ab)
     {
         playerSkillController.ToggleHighJump();
     }
@@ -53,7 +53,7 @@ public class PlayerPunCallBack :Player
         playerUI.UpdateSkill(activeSkill);
     }
     [PunRPC]
-    public void SetLowGravityOther()
+    public void SetLowGravityOther(int ab)
     {
         playerSkillController.ToggleLowGravity();
     }
